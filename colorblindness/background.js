f234fa94d6568d4bb8714ab5435621e0f0544f9b
Sameer Sinha <78034576+sameersinha3@users.noreply.github.com>
2023-10-07 22:54:05 -0400

    Delete colorblindness directory

--- colorblindness/background.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener(execScript);
-
-async function execScript() {
-    chrome.scripting.executeScript({
-        target: { tabId: chrome.window.getCurrent, allFrames: true},
-        files: ["listener.js"]
-    });
-}
